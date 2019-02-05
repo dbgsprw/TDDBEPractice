427a59a4bd7a3d8c5c5cca7112edf54f7aa96cb9
dbgsprw <dbgsprw@gmail.com>
2019-02-06 01:47:59 +0900

    Change Equals Method

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,9 +1,10 @@
 class Dollar {
-    int amount;
+    private int amount;
 
     Dollar (int amount) {
         this.amount = amount;
     }
+
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
