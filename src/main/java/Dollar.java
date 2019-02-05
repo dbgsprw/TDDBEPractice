e90d1d2685786e47d70c8942963f0c40e2d494e6
dbgsprw <dbgsprw@gmail.com>
2019-02-06 02:15:27 +0900

    Add Franc

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,7 +1,6 @@
-class Dollar {
-    private int amount;
+class Dollar extends Money {
 
-    Dollar (int amount) {
+    Dollar(int amount) {
         this.amount = amount;
     }
 
@@ -9,8 +8,4 @@ class Dollar {
         return new Dollar(amount * multiplier);
     }
 
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
-    }
 }
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -13,5 +13,8 @@ public class MoneyTest {
     public void testEquality() {
         Assert.assertTrue(new Dollar(5).equals(new Dollar(5)));
         Assert.assertFalse(new Dollar(5).equals(new Dollar(6)));
+        Assert.assertTrue(new Franc(5).equals(new Franc(5)));
+        Assert.assertFalse(new Franc(5).equals(new Franc(6)));
+        Assert.assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 }
