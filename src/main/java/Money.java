b0c3a5ce2d01cfbeca48a12d7c08e2cebfc5319b
dbgsprw <dbgsprw@gmail.com>
2019-02-06 02:31:44 +0900

    Use Factory Pattern

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,5 +1,11 @@
-class Money {
+abstract class Money {
     protected int amount;
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    abstract Money times(int multiplier);
 
     public boolean equals(Object object) {
         Money money = (Money) object;
