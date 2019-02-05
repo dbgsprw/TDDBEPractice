784da77e8ea8f9c3cafd3250fa6a9528b7238998
dbgsprw <dbgsprw@gmail.com>
2019-02-06 03:16:47 +0900

    Add Currency Concept

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -13,8 +13,14 @@ public class MoneyTest {
     public void testEquality() {
         Assert.assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         Assert.assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        Assert.assertTrue(new Franc(5).equals(new Franc(5)));
-        Assert.assertFalse(new Franc(5).equals(new Franc(6)));
-        Assert.assertFalse(new Franc(5).equals(Money.dollar(5)));
+        Assert.assertTrue(Money.franc(5).equals(Money.franc(5)));
+        Assert.assertFalse(Money.franc(5).equals(Money.franc(6)));
+        Assert.assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
+
+    @Test()
+    public void testCurrency() {
+        Assert.assertEquals("USD", Money.dollar(1).currency());
+        Assert.assertEquals("CHF", Money.franc(1).currency());
     }
 }
